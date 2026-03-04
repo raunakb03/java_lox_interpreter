946a2cf1fc3db0ccff023f386e4c73dc46ec57ba
Raunak Bhalotia <raunakbhalotia0308@gmail.com>
2026-03-04 17:23:01 +0530

    Create main class, token and token type

---
+++ Scanner.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Scanner {
+    private final String source;
+    private final List<Token> tokens = new ArrayList<>();
+
+    Scanner(String source) {
+        this.source = source;
+    }
+
+    List<Token> scanTokens() {
+    }
+}
